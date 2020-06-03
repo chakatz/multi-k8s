3face37c06762005c4c8bb06adedc67a2483b995
Chaim Keren-Tzion <chaim@intercomp.co.il>
2020-06-03 22:58:01 +0300

    deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t chakatz/multi-client:latest -t chakatz/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t chakatz/multi-server:latest -t chakatz/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t chakatz/multi-worker:latest -t chakatz/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push chakatz/multi-client:latest
+docker push chakatz/multi-server:latest
+docker push chakatz/multi-worker:latest
+
+docker push chakatz/multi-client:$SHA
+docker push chakatz/multi-server:$SHA
+docker push chakatz/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image Deployment/client-deployment client=chakatz/multi-client:$SHA
+kubectl set image Deployment/server-deployment client=chakatz/multi-server:$SHA
+kubectl set image Deployment/worker-deployment client=chakatz/multi-worker:$SHA
